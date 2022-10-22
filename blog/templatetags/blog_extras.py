ba15add6b733ddf6f1fd071367c9316693317a08
Salokhiddin Khayrullaev <skhayrullayev@gmail.com>
2022-10-22 19:42:35 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,29 +4,53 @@ from django import template
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
+from blog.models import Post
+
 register = template.Library()
 
 @register.filter
-def author_details(author, current_user=None):
+def author_details(author, current_user):
 
 	if not isinstance(author, get_user_model()):
-		return ""
+			# return empty string as safe default
+			return ""
 
 	if author == current_user:
-		return format_html("<strong>me</strong>")
+			return format_html("<strong>me</strong>")
 
 	if author.first_name and author.last_name:
-		name = f"{author.first_name} {author.last_name}"
+			name = f"{author.first_name} {author.last_name}"
 	else:
-		name = f"{author.username}"
+			name = f"{author.username}"
 
 	if author.email:
-		prefix = format_html("<a href='mailto:{}'>", author.email)
-		suffix = format_html("</a>")
+			prefix = format_html('<a href="mailto:{}">', author.email)
+			suffix = format_html("</a>")
 	else:
-		prefix = ""
-		suffix = ""
+			prefix = ""
+			suffix = ""
 
-	current_user = request.user
-	
-	return format_html("{}{}{}", prefix, name, suffix)
+	return format_html('{}{}{}', prefix, name, suffix)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:3]
+    return {"title": "Recent Posts", "posts": posts}
+
+
+@register.simple_tag
+def row(extra_classes=""):
+	return format_html(f'<div class="row {extra_classes}">')
+
+@register.simple_tag
+def endrow():
+	return format_html("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=""):
+	return format_html(f'<div class="col {extra_classes}">')
+
+@register.simple_tag
+def endcol():
+	return format_html(f'</div>')
