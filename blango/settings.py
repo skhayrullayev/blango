bd2971a5de53b2f486df1edc671ea3d2984c65e9
Salokhiddin Khayrullaev <skhayrullayev@gmail.com>
2022-12-25 13:00:44 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -51,9 +51,11 @@ class Dev(Configuration):
 			'blog',
 			'crispy_forms',
 			'crispy_bootstrap5',
+			"debug_toolbar",
 	]
 
 	MIDDLEWARE = [
+			"debug_toolbar.middleware.DebugToolbarMiddleware",
 			'django.middleware.security.SecurityMiddleware',
 			'django.contrib.sessions.middleware.SessionMiddleware',
 			'django.middleware.common.CommonMiddleware',
@@ -189,3 +191,5 @@ class Dev(Configuration):
 					"level": "DEBUG",
 			},
 	}
+
+	INTERNAL_IPS = ["192.168.10.93"]
--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
@@ -40,3 +40,6 @@ def post_detail(request, slug):
 	context = {"post": post, 'comment_form':comment_form}
 	return render(request, "blog/post-detail.html", context)
 
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
